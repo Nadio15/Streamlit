ca3c55ac8ac9298c97b806c55d881e828477537f
Nadio Alferinanda <59412695+Nadio15@users.noreply.github.com>
2025-09-22 15:01:18 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -108,7 +108,7 @@ region_colors = {
     "JAVA": "#DC143C",
     "KALISUMAPA": "#66FF00",
     "SUMATERA": "#FFD700",
-    "NATIONAL": "#C0C0C0",
+    "NATIONAL": "#FFFFFF",
     "Part 75 from MW": "#FF00FF",
     "Part 75 from SP": "#03C03C",
     "75 Blended": "#0000FF"
@@ -286,4 +286,5 @@ st.download_button(
     file_name="dashboard_filtered.xlsx",
     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 )
+
 
